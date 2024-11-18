2ced694996cb5c55717b5025a4ad9c9a640d56c3
chim7a <chimalawrence11@gmail.com>
2024-11-18 13:18:16 +0100

    Delete document.

--- controllers/delete-account.js
+++ controllers/delete-account.js
@@ -1,5 +1,20 @@
+import validator from "validator";
+import Students from "../models/userModel.js";
+
 async function deleteAccount(req, res) {
-  res.status(200).json({ message: "Deleted successfully" });
+  const { email } = req.query;
+  // Check for email provided
+  if (!email || !validator.isEmail(email)) {
+    return res.status(400).json({ message: "Please provide valid Email." });
+  }
+  try {
+    const studentToBeDeleted = await Students.deleteOne({ email: email });
+    console.log(studentToBeDeleted);
+    res.status(200).json({ message: "User account Deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({ message: "Server error occured" });
+  }
 }
 
 export default deleteAccount;
